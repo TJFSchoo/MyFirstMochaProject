899de161217fd31433f58e4495ae2eb874db340c
TJFSchoo <tomschoonbeek@gmail.com>
2018-04-22 16:06:08 +0200

    Added routes folder with v1 and v2 API (./api/v1)

--- server.js
+++ server.js
@@ -7,26 +7,39 @@ let config = require('./config.json');
 app.set('PORT', config.webPort);
 let port = process.env.PORT || app.get('PORT');
 
-app.get('/', function(request, response) {
-    response.send('Hello Avans!');
-});
-app.get('/about', function(request, response) {
-    response.send('Written by Tom Schoonbeek, Klas D');
-});
-app.post('/', function(request, response) {
-    response.send('Hello Avans, POST request received!');
-});
-app.put('/', function(request, response) {
-    response.send('Hello Avans, PUT request received!');
-});
-app.all('/api', function(request, response) {
-    response.send('API call!');
-});
 
-app.all('*', function(request, response) {
-    response.status(404);
-    response.send('404 - Not found');
+
+
+app.all('*', function(request, response, next) {
+    console.log(request.method + " " + request.url);
+    next();
 });
+app.use('/api/v1', require('./routes/routes_api_v1'));
+
+
+//
+// app.get('/', function(request, response) {
+//     response.send('Hello Avans!');
+// });
+// app.get('/about', function(request, response) {
+//     response.send('Written by Tom Schoonbeek, Klas D');
+// });
+// app.post('/', function(request, response) {
+//     response.send('Hello Avans, POST request received!');
+// });
+// app.put('/', function(request, response) {
+//     response.send('Hello Avans, PUT request received!');
+// });
+// app.all('/api', function(request, response) {
+//     response.send('API call!');
+// });
+//
+// app.all('*', function(request, response) {
+//     response.status(404);
+//     response.send('404 - Not found');
+// });
+
+
 app.listen(port, function() {
     console.log('Server app is listening on port ' + port);
 });
