cab252bc95a3902935e1529f23a08a93fc89e37b
TJFSchoo <tomschoonbeek@gmail.com>
2018-04-22 22:17:47 +0200

    Added recipe number query parameter (/recipes/2 for example)

--- routes/routes_api_v2.js
+++ routes/routes_api_v2.js
@@ -16,6 +16,21 @@ router.get('/recipes', function(request, response) {
     response.json(recipes);
 });
 
+router.get('/recipes/:number/', function(request, response) {
+    let result = [];
+    let number = request.params.number || '';
+
+    if(number === ''){
+        result ={"Result": "Recipe number not found."};
+    } else {
+        result = recipes[number];
+    }
+
+    response.status(200);
+    response.json(result);
+
+});
+
 router.get('*', function(request, response) {
     response.status(404);
     response.json({
