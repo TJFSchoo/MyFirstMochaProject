f246c117bec3915396274a6efe149320bd534f0b
TJFSchoo <tomschoonbeek@gmail.com>
2018-04-22 15:36:38 +0200

    Added config.json with local port, added functionality in server.js

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 
+// Het gebruiken van config.json met daarin de goede lokale port, toewijzen aan variabele 'port'
+let config = require('./config.json');
+app.set('PORT', config.webPort);
+let port = process.env.PORT || app.get('PORT');
 
 app.get('/', function(request, response) {
     response.send('Hello Avans!');
-})
+});
 app.get('/about', function(request, response) {
     response.send('Written by Tom Schoonbeek, Klas D');
-})
+});
 app.post('/', function(request, response) {
     response.send('Hello Avans, POST request received!');
-})
+});
 app.put('/', function(request, response) {
     response.send('Hello Avans, PUT request received!');
-})
+});
 app.all('/api', function(request, response) {
     response.send('API call!');
-})
+});
 
 app.all('*', function(request, response) {
     response.status(404);
     response.send('404 - Not found');
-})
-
-
-
-
-app.listen(3000, function() {
-    console.log('Server app is listening on port 3000');
-})
+});
+app.listen(port, function() {
+    console.log('Server app is listening on port ' + port);
+});
